c5c21f7f01b2255089c4884b16e2af387a959b12
Anson Varughese <avarugh1@binghamton.edu>
2016-12-03 22:56:14 -0500

    Adding comments

--- src/KMPSearch.cpp
+++ src/KMPSearch.cpp
@@ -3,75 +3,65 @@
 KMPSearch::KMPSearch(std::string textP, std::string patternP){
 	text = textP;
 	pattern = patternP;
-	pSize = pattern.size();
+	pSize = pattern.size(); //easier to type vars
 	tSize = text.size();
 	suffixArr = new int[pSize](); //auto initialize to 0
 
 	computeSuffixArr();
 }
 
+//easy printing function
 void KMPSearch::printSuffixArray(){
 	std::cout << "~~~Printing suffix arr~~~"<<std::endl;
 	for(int i = 0; i < pSize; i++)
 		std::cout<<suffixArr[i]<< " ";
-	std::cout << "\nDone"<<std::endl;
+	std::cout << "\n~~~Done Printing~~~ "<<std::endl;
 }
 
+//computing prefix-suffix array for KMP Search
 void KMPSearch::computeSuffixArr(){
 	int j=0;
 	for(int i=1; i<pSize;i++){
-		if(pattern[i] == pattern[j])
+		if(pattern[i] == pattern[j]) 
 			suffixArr[i] = ++j;
-		else if(j != 0){
-			i--; // preventing for-loop increment
+		else if(j != 0){ //if not at beginning of pattern - use prefix-suffix
+			i--; 	//array to move locations and prevent loop increment
 			j=suffixArr[j-1];
-		}
+		}//else increment i and move on 
 	}
 	//printSuffixArray();
-
-	/*while(i < pSize){
-		if(pattern[i] == pattern[j]){
-			suffixArr[i] = ++j;
-			i++;
-		}else{
-			if(j != 0){
-				j = suffixArr[j-1];
-			}else{
-				suffixArr[i] = 0;
-				i++;
-			}
-		}
-	}*/
-	
 }
 
+//actual KMP algorithm after preSuff array created
 bool KMPSearch::KMPSearchHelper(int &tIndex){
 		int pIndex = 0; //position in pattern
-		while((tIndex < tSize) && (pIndex < pSize)){
+		while((tIndex < tSize) && (pIndex < pSize)){ //if at the end - ret
 			if(text[tIndex] == pattern[pIndex]){
 				tIndex++;
 				pIndex++;
-			}else if(pIndex != 0){
+			}else if(pIndex != 0){ //go to Arr for quicker increments
 				pIndex = suffixArr[pIndex-1];
-			}else{
+			}else{	//move on with text - prefSuffArr won't help
 				tIndex++;
 			}
 		}
-		return (pIndex == pSize);
-}
+		return (pIndex == pSize); //did we find a location? 
+}								//it's stored in tIndex
 
+//finds all locations of pattern
 std::vector<int> KMPSearch::findAll(){
 	std::vector<int> locations;
 	int tIndex = 0;
 	bool found = true;
-	while(found){
+	while(found){ //find all locations
 		found = KMPSearchHelper(tIndex);
 		if(found)
-			locations.push_back(tIndex - pSize);
+			locations.push_back(tIndex - pSize); // fixing index
 	}
 	return locations;
 }
 
+//finds first location of pattern
 int KMPSearch::search(){
 	int tIndex = 0;
 	bool found = KMPSearchHelper(tIndex);
@@ -81,6 +71,7 @@ int KMPSearch::search(){
 		return -1;
 }
 
+//cleaning up prefix suffix arr
 KMPSearch::~KMPSearch(){
 	delete[] suffixArr;
 }
